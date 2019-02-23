8b37b6adf7090eff68143344a93918e46fb39a44
Sparky <17860180+sparkins@users.noreply.github.com>
2019-02-22 22:53:47 -0800

    updated instructions

--- clickygameapp/src/components/Banner.js
+++ clickygameapp/src/components/Banner.js
@@ -9,10 +9,10 @@ class Banner extends React.Component {
         return (
             <Container>
                 <h1 className='header'> Playing the game: </h1>
-                <h2>- You earn points by clicking on the large emoji images below, but you must be careful and avoid choosing an
-                image that you have already picked. </h2>
-                <h2>- The game ends when you either click on all the images, or
-                by clicking an image a second time.</h2>
+                <h3>** THIS IS A MEMORY GAME - where you score points for clicking on the large emoji icons below.</h3>
+                <h3>** THE OBJECT OF THE GAME - is to click all 12 emoji icons just once, but beware they shuffle after each click.</h3>
+                <h3>** THE GAME ENDS - when you either click on all the images, or
+                by clicking an image a second time.  GOOD LUCK !!!</h3>
             </Container>
         )
     }
