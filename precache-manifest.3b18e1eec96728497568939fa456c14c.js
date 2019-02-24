a2a29a92649f46a5ac87c82efa393fb0dd217468
Sparky <17860180+sparkins@users.noreply.github.com>
2019-02-23 22:49:33 -0800

    Updates

---
+++ precache-manifest.3b18e1eec96728497568939fa456c14c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "d867eb8bf6f8c27edfcd581d19b42b38",
+    "url": "/ClickyGame/static/media/emoji-love-heart.d867eb8b.png"
+  },
+  {
+    "revision": "b3e6a2e3a7ca3b6cba76",
+    "url": "/ClickyGame/static/css/main.989edd86.chunk.css"
+  },
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "cdf5ee7010bce9fcf2f6",
+    "url": "/ClickyGame/static/js/2.cdf5ee70.chunk.js"
+  },
+  {
+    "revision": "2b245ee2fe44631917643b6312962279",
+    "url": "/ClickyGame/static/media/Emoji-background.2b245ee2.png"
+  },
+  {
+    "revision": "e56d96c238d0baaa95eeebcd92ebabfb",
+    "url": "/ClickyGame/static/media/emoji-anger.e56d96c2.png"
+  },
+  {
+    "revision": "923c7f8080b18d8654cc174d94c1f76f",
+    "url": "/ClickyGame/static/media/emoji-big-eyes.923c7f80.png"
+  },
+  {
+    "revision": "83013472701ce476a4e9e6401a6058ad",
+    "url": "/ClickyGame/static/media/emoji-blushing.83013472.png"
+  },
+  {
+    "revision": "3626a96fe8eff6a1a32f60cad6794ed2",
+    "url": "/ClickyGame/static/media/emoji-crying.3626a96f.png"
+  },
+  {
+    "revision": "39d120c77f36a0dca8605f5c8b03f6f4",
+    "url": "/ClickyGame/static/media/emoji-goofy.39d120c7.png"
+  },
+  {
+    "revision": "b3e6a2e3a7ca3b6cba76",
+    "url": "/ClickyGame/static/js/main.b3e6a2e3.chunk.js"
+  },
+  {
+    "revision": "5d08eb15adde606e6705f4dd03f9d6fe",
+    "url": "/ClickyGame/static/media/emoji-nerd-glasses.5d08eb15.png"
+  },
+  {
+    "revision": "d8630d7d6aa67abbad8a2805a7aed9d8",
+    "url": "/ClickyGame/static/media/emoji-sadness.d8630d7d.png"
+  },
+  {
+    "revision": "ee94e15e450eb2e7eb6cf33fc16ddbc7",
+    "url": "/ClickyGame/static/media/emoji-tears-of-joy.ee94e15e.png"
+  },
+  {
+    "revision": "2223b6b0f81faf879079a1c419821114",
+    "url": "/ClickyGame/static/media/emoji-teethy-grin.2223b6b0.png"
+  },
+  {
+    "revision": "c73674bb0d585b4172cd0d8c15c5af34",
+    "url": "/ClickyGame/static/media/emoji-the-thinker.c73674bb.png"
+  },
+  {
+    "revision": "b5ddf227ed91cd296c1bba37a8055f01",
+    "url": "/ClickyGame/static/media/emoji-winking.b5ddf227.png"
+  },
+  {
+    "revision": "8abdd484472d840e4af88ee972caca23",
+    "url": "/ClickyGame/static/media/emoji-zipper-mouth.8abdd484.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "36eed17b307f2a6410320bd192d5d929",
+    "url": "/ClickyGame/index.html"
+  }
+];
